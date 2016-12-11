62baa9314e3b3139e0648cc90d6bb005719fac30
Subodh Kamble <skamble@brocade.com>
2016-12-12 00:11:11 +0530

    Changed path of workspace dir

--- workspace/c/extern-global/example2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int global_4 = 40;
-
-void func2()
-{
-    printf("global_4 :: %u \n", global_4);
-}
--- workspace/c/multicast/multicast.h
+++
@@ -1,4 +0,0 @@
-#define M_CAST_IP   "230.0.0.10"
-#define M_CAST_PORT (9099)
-#define M_CAST_GRP  "Test Group"
-
